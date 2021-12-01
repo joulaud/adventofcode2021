07c66dac8e61dd02180d572165923bbdc47b6a95
Francois Joulaud <francois.joulaud@radiofrance.com>
2021-12-01 10:58:01 +0100

    day 01 part2: YIPEE

--- day01/second.scm
+++ day01/second.scm
@@ -1,7 +1,13 @@
 (define-module (adventofcode first)
     #:use-module (ice-9 rdelim))
 
-(define (count-increments acc last)
+(define (anyfalse a b c)
+    (or
+     (equal? a #f)
+     (equal? b #f)
+     (equal? c #f)))
+
+(define (count-increments acc p1 p2 p3)
     (let ((line (read-line)))
       (cond
        ((eof-object? line)
@@ -12,14 +18,14 @@
                   ((equal? num #f)
                    ;; not a number, ignore
                    (count-increments acc last))
-                  ((equal? last #f)
-                   ;; no previous measurements
-                   (count-increments acc num))
-                  ((> num last)
+                  ((anyfalse p1 p2 p3)
+                   ;; lacking at least one measurment
+                   (count-increments acc p2 p3 num))
+                  ((> (+ p2 p3 num) (+ p1 p2 p3))
                    ;; detected increment, increase accumulator
-                   (count-increments (+ acc 1) num))
+                   (count-increments (+ acc 1) p2 p3 num))
                   (#t
                     ;; else, no increment just parse next line
-                    (count-increments acc num))))))))
+                    (count-increments acc p2 p3 num))))))))
 
-(display (count-increments 0 #f))
+(display (count-increments 0 #f #f #f))
