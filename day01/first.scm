555225eccf7182985ae82e4baa4c0beafbbee478
Francois Joulaud <francois.joulaud@radiofrance.com>
2021-12-01 10:44:23 +0100

    day01: first exercice (with bug)

---
+++ day01/first.scm
@@ -0,0 +1,26 @@
+(define-module (adventofcode first)
+    #:use-module (ice-9 rdelim))
+
+(define (count-increments acc last)
+    (let ((line (read-line)))
+      (cond
+       ((eof-object? line)
+        ;; end of file, return final result
+        acc)
+       (else (let ((num (string->number line)))
+               (begin
+                 (cond
+                  ((equal? num #f)
+                   ;; not a number, ignore
+                   (count-increments acc last))
+                  ((equal? last #f)
+                   ;; no previous measurements
+                   (count-increments acc num))
+                  ((> num last)
+                   ;; detected increment, increase accumulator
+                   (count-increments (+ acc 1) num))
+                  (#t
+                    ;; else, no increment just parse next line
+                    (count-increments acc num)))))))))
+
+(display (count-increments 0 0))
