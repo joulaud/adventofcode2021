32ac2b70ded6627fe76c1e3f8980d9a808ea318d
Francois Joulaud <francois.joulaud@radiofrance.com>
2021-12-05 21:09:44 +0100

    day03: trying to put test framework in place

---
+++ day03/first.scm
@@ -0,0 +1,42 @@
+(define-module (first)
+    #:use-module (ice-9 rdelim)
+    #:use-module (ice-9 match)
+    #:use-module (srfi srfi-9)
+    #:use-module (srfi srfi-9 gnu) ; immutable records
+    #:use-module (srfi srfi-41))
+
+(define stream-of-lines
+  (stream-lambda ()
+     (let
+         ((line (read-line)))
+      (cond
+       ((eof-object? line) stream-null)
+       (else (stream-cons line (stream-of-lines)))))))
+
+(define-immutable-record-type <binary-counter>
+    (make-binary-counter zeros ones)
+    binary-counter?
+    (zeros count0 set-count0)
+    (ones count1 set-count1))
+
+(define binary-counter-empty (make-binary-counter 0 0))
+
+(define (inc0 counter)
+    (set-count0
+     counter
+     (+ 1 (count0 counter))))
+
+(define (inc1 counter)
+    (set-count1
+     counter
+     (+ 1 (count1 counter))))
+
+(define (add-char-to-counter counter c)
+    (cond
+     ((char=? #\0 c) (inc0 counter))
+     ((char=? #\1 c) (inc1 counter))))
+
+(define)
+
+(define-public (main args)
+  (format #t "UNIMPLEMENTED"))
---
+++ day03/tests.scm
@@ -0,0 +1,42 @@
+(define-module (tests first)
+    #:use-module (first)
+    #:use-module (srfi srfi-19)
+    #:use-module (srfi srfi-64))
+
+;; import internal functions of module to test
+(define binary-counter-empty (@@ (first) binary-counter-empty))
+(define count0 (@@ (first) count0))
+(define count1 (@@ (first) count1))
+(define inc0 (@@ (first) inc0))
+(define inc1 (@@ (first) inc1))
+
+(test-begin "binary-counter functions")
+(test-eqv (count0 binary-counter-empty) 0)
+(test-eqv (count1 binary-counter-empty) 0)
+(define one1 (inc1 binary-counter-empty))
+(define one0 (inc0 binary-counter-empty))
+(test-eqv (count0 one1) 0)
+(test-eqv (count1 one1) 1)
+(test-eqv (count0 one0) 1)
+(test-eqv (count1 one0) 0)
+(test-end "binary-counter functions")
+
+;; import internal functions of module to test
+(define add-char-to-counter (@@ (first) add-char-to-counter))
+
+(test-begin "char to binary-counter functions")
+(define a (add-char-to-counter binary-counter-empty #\0))
+(define b (add-char-to-counter binary-counter-empty #\0))
+(define c (add-char-to-counter binary-counter-empty #\1))
+(test-eqv (count0 a) 1)
+(test-eqv (count1 a) 0)
+(test-eqv (count0 b) 2)
+(test-eqv (count1 b) 0)
+(test-eqv (count0 c) 2)
+(test-eqv (count1 c) 1)
+(test-end"char to binary-counter functions")
+
+(define (x y)
+    (cond
+          ((eqv? y 1) 1)))
+(x 2)
