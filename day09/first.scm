834d9055a75205ec5fb19237ea6620d69df8eacc
Francois Joulaud <francois.joulaud@radiofrance.com>
2021-12-09 22:02:40 +0100

    day9: first part

---
+++ day09/first.scm
@@ -0,0 +1,142 @@
+(define-module (first)
+    #:use-module (ice-9 rdelim)
+    #:use-module (ice-9 match)
+    #:use-module (ice-9 format)
+    #:use-module (srfi srfi-1) ; lists library
+    #:use-module (srfi srfi-11) ; let*-values
+    #:use-module (srfi srfi-9)
+    #:use-module (srfi srfi-9 gnu) ; immutable records
+    #:use-module (srfi srfi-41) ; Streams
+    #:use-module (srfi srfi-43)) ; Vectors iterators
+
+(use-modules (ice-9 rdelim))
+(use-modules (ice-9 match))
+(use-modules (ice-9 format))
+(use-modules (srfi srfi-1)) ; lists library
+(use-modules (srfi srfi-11)) ; let*-values
+(use-modules (srfi srfi-9))
+(use-modules (srfi srfi-9 gnu)) ; immutable records
+(use-modules (srfi srfi-41))
+
+(define (dbg t v) (format #t "~s: ~a\n" t v) (force-output))
+
+(define stream-of-lines
+  (stream-lambda (port)
+     (let
+         ((line (read-line port)))
+      (cond
+       ((eof-object? line) stream-null)
+       (else (stream-cons line (stream-of-lines port)))))))
+
+(define (parse-line line)
+   (let* (
+          (x (string->list line))
+          (x (map char->integer x))
+          (x (map (lambda (v) (- v (char->integer #\0))) x)))
+     x))
+
+(define (stream-of-lines->array strm)
+   (let* ((x (stream-map parse-line strm))
+          (x (stream->list x))
+          (x (list->array 2 x)))
+     x))
+
+(define (neighbours l c maxl maxc)
+  (filter
+     (lambda (x) (begin
+                  (and x (not (equal? x (cons l c))))))
+     (append-map
+       (lambda (ld)
+          (let ((lres (- l ld)))
+            (if (and (>= lres 0) (< lres maxl))
+                (map (lambda (cd)
+                        (let ((cres (- c cd)))
+                           (if (and (>= cres 0) (< cres maxc))
+                               (cons lres cres)
+                               #f)))
+                     '(-1 0 1))
+                '())))
+       '(-1 0 1))))
+
+
+;;; (define (lower a l c)
+;;;    (fold
+;;;       (lambda (cur acc)
+;;;          (let ((curval (array-ref a (car cur) (cdr cur))))
+;;;            (if (not acc)
+;;;                curval
+;;;                (min curval acc))))
+;;;     #f
+;;;     (let* ((dims (array-dimensions a))
+;;;            (maxl (car dims))
+;;;            (maxc (cadr dims)))
+;;;        (dbg "l" l)
+;;;        (dbg "c" c)
+;;;        (dbg "maxl" maxl)
+;;;        (dbg "maxc" maxc)
+;;;        (neighbours l c maxl maxc))))
+
+(define (low? a l c)
+  (let* ((dims (array-dimensions a))
+         (maxl (car dims))
+         (maxc (cadr dims))
+         (thisval (array-ref a l c)))
+     (let loop ((neighbours (neighbours l c maxl maxc)))
+            (if (eqv? '() neighbours)
+                #t
+                (let* ((curpos (car neighbours))
+                       (curval (array-ref a (car curpos) (cdr curpos))))
+                   (if (<= curval thisval)
+                       #f
+                       (loop (cdr neighbours))))))))
+
+(define (array-for-each-index a proc-cell proc-endofline)
+  (let* ((dims (array-dimensions a))
+         (maxl (car dims))
+         (maxc (cadr dims)))
+    (do ((i 0 (1+ i)))
+        ((>= i maxl))
+      (do ((j 0 (1+ j)))
+          ((>= j maxc))
+        (proc-cell i j (array-ref a i j)))
+      (proc-endofline i))))
+
+(define (print-array a)
+  (array-for-each-index
+       a
+       (lambda (i j v) (display v))
+       (lambda (i) (display "\n"))))
+
+(define (all-lows a)
+  (let* ((dims (array-dimensions a))
+         (maxl (car dims))
+         (maxc (cadr dims))
+         (out (make-array 'u maxl maxc)))
+    (array-for-each-index
+         a
+         (lambda (i j v) (array-set! out (if (low? a i j) 'x 'n) i j))
+         (lambda (i) #f))
+    out))
+
+(define (risk-level a)
+  (let* ((dims (array-dimensions a))
+         (maxl (car dims))
+         (maxc (cadr dims))
+         (out 0))
+    (array-for-each-index
+         a
+         (lambda (i j v) (set! out (if (low? a i j) (+ out 1 v) out)))
+         (lambda (i) #f))
+    out))
+
+(define-public (main args)
+  (let* (
+         (strm (stream-of-lines (current-input-port)))
+         (a (stream-of-lines->array strm))
+         (result1 (risk-level a))
+         (result2 "UNIMP"))
+    (format #t "result1: ~a\n" result1)
+    (format #t "result2: ~a\n" result2)))
+(define input-strm (stream-of-lines (open-input-string input-string)))
+
+
---
+++ day09/tests.scm
@@ -0,0 +1,71 @@
+(define-module (tests first)
+    #:use-module (first)
+    #:use-module (ice-9 rdelim)
+    #:use-module (srfi srfi-19)
+    #:use-module (srfi srfi-41)
+    #:use-module (srfi srfi-43)
+    #:use-module (srfi srfi-64))
+
+(use-modules (srfi srfi-1))
+(define (dbg t v) (format #t "~s: ~a\n" t v) (force-output))
+
+(define input-string "2199943210
+3987894921
+9856789892
+8767896789
+9899965678")
+
+(define example-array
+    #2((2 1 9 9 9 4 3 2 1 0)
+       (3 9 8 7 8 9 4 9 2 1)
+       (9 8 5 6 7 8 9 8 9 2)
+       (8 7 6 7 8 9 6 7 8 9)
+       (9 8 9 9 9 6 5 6 7 8)))
+
+(define stream-of-lines  (@@ (first) stream-of-lines))
+(define input-strm (stream-of-lines (open-input-string input-string)))
+
+;; import internal functions of module to test
+(define parse-line (@@ (first) parse-line))
+(define stream-of-lines->array (@@ (first) stream-of-lines->array))
+
+(test-begin "parsing input")
+(test-equal '(1 2 3 4 5) (parse-line "12345"))
+(test-equal
+ example-array
+ (stream-of-lines->array input-strm))
+(test-end "parsing input")
+
+(test-begin "neighbours list")
+(define neighbours  (@@ (first) neighbours))
+(test-equal 8 (length (neighbours 1 1 3 3)))
+(test-equal 3 (length (neighbours 0 0 3 3)))
+(test-equal 5 (length (neighbours 1 2 3 3)))
+(test-end "neighbours list")
+
+;(define x (@@ (first) x))
+(define low? (@@ (first) low?))
+
+(test-begin "low points")
+(test-equal #t (low? example-array 0 1))
+(test-equal #t (low? example-array 0 1))
+(test-equal #f (low? example-array 0 0))
+(test-equal #f (low? example-array 2 4))
+(test-end "low points")
+
+;; import internal functions of module to test
+(define risk-level  (@@ (first) risk-level))
+
+(test-begin "risk-level")
+(test-equal 15 (risk-level example-array))
+(test-end "risk-level")
+
+
+
+
+
+
+
+
+
+
