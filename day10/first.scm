c3959887657bb3af86e5a14b27d4cdb0d9f4b051
Francois Joulaud <francois.joulaud@radiofrance.com>
2021-12-11 03:04:18 +0100

    day10: YIPEEE

--- day10/first.scm
+++ day10/first.scm
@@ -22,6 +22,12 @@
 (define (dbg t v) (format #t "~s: ~a\n" t v) (force-output))
 
 
+(define-record-type <analysis>
+  (make-analysis err opened)
+  analysis?
+  (err analysis-err)
+  (opened analysis-opened))
+
 
 (define stream-of-lines
   (stream-lambda (port)
@@ -30,9 +36,6 @@
       (cond
        ((eof-object? line) stream-null)
        (else (stream-cons line (stream-of-lines port)))))))
-
-(define (dbg t v) (format #t "~s: ~a\n" t v) (force-output))
-
 
 (define pair-chars '(
                      (#\( . #\))
@@ -48,16 +51,16 @@
    (eqv? #\< c)
    (eqv? #\< c)))
 
-(define (line-first-error line)
+(define (line-status line)
   (let loop ((line (string->list line))
              (opened '()))
        (cond
-          ((eqv? '() line) #f)
+          ((eqv? '() line) (make-analysis #f opened))
           ((open-pair-char? (car line))
            (loop (cdr line) (cons (car line) opened)))
           ((eqv? (car line) (cdr (assoc (car opened) pair-chars)))
            (loop (cdr line) (cdr opened)))
-          (else (car line)))))
+          (else (make-analysis (car line) opened)))))
 
 (define (char->score c)
  (cdr
@@ -71,17 +74,62 @@
 
 (char->score #f)
 
-(define (err-score)
-   (let loop ((total 0) (line (read-line)))
-     (if (eof-object? line) total
-         (let* ((err (line-first-error line))
-                (cur-score (char->score err)))
-            (loop (+ cur-score total) (read-line))))))
+(define (char->autocomplete-score c)
+ (cdr
+  (assoc c
+   '(
+      (#f . 0)
+      (#\( . 1)
+      (#\[ . 2)
+      (#\{ . 3)
+      (#\< . 4)))))
 
+(char->autocomplete-score #f)
+(char->autocomplete-score (car (string->list "(")))
+(char->autocomplete-score (car (string->list "[")))
+(char->autocomplete-score (car (string->list "{")))
+(char->autocomplete-score (car (string->list "<")))
+
+
+(define-record-type <result>
+  (mkres errscore autocompletescores)
+  result?
+  (errscore res-err)
+  (autocompletescores res-auto))
+
+
+(define (err-score)
+   (let loop ((total 0) (autocompletes '()) (line (read-line)))
+     (if (eof-object? line) (mkres total autocompletes)
+         (let* ((analysis (line-status line))
+                (cur-err (analysis-err analysis))
+                (cur-errscore (char->score cur-err))
+                (cur-autocomplete (analysis-opened analysis))
+                (cur-autocompletescores (map char->autocomplete-score cur-autocomplete))
+                (cur-autocompletescore (fold
+                                        (lambda (cur acc) (+ cur (* 5 acc)))
+                                        0 cur-autocompletescores))
+                (cur-autocompletescore (if cur-err #f cur-autocompletescore)))
+            (loop (+ cur-errscore total) (cons cur-autocompletescore autocompletes) (read-line))))))
+
+;(with-input-from-string "[({(<(())[]>[[{[]{<()<>>" err-score)
+
+(with-input-from-string "[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]" err-score)
 
 (define-public (main args)
   (let* (
-         (result1 (err-score))
-         (result2 "UNIMP"))
+         (result (err-score))
+         (result1 (res-err result))
+         (myresauto (filter identity (res-auto result)))
+         (result2 (list-ref (sort myresauto <=) (quotient (length myresauto) 2))))
     (format #t "result1: ~a\n" result1)
     (format #t "result2: ~a\n" result2)))
