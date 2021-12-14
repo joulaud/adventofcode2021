ff20fb65435ca5c25b6bce48837c93f3c15350e1
Francois Joulaud <francois.joulaud@radiofrance.com>
2021-12-14 19:10:10 +0100

    day14: final refacto ? I give up

--- day14/polymers2.scm
+++ day14/polymers2.scm
@@ -43,57 +43,6 @@
 (define (pair-b pair)
   (string-ref (pair-l pair) 1))
 
-(define (molecule2->string molecule)
-  (let* ((pairs (molecule2-pairs molecule)))
-    (pairs->string pairs)))
-
-(define (pairs->string pairs)
-      (fold
-         (lambda (x acc)
-           (let* ((pair (car x))
-                  (val (cdr x))
-                  (a (pair-a pair))
-                  (b (pair-b pair)))
-             (string-append
-              acc (list->string (list a b))
-              ": " (number->string val) "\n")))
-         ""
-         pairs))
-
-(define (mysort l)
-  (sort
-    l
-    (lambda (x y)
-        (cond
-          ((= (cdr x) (cdr y))
-           (if (char=?  (pair-a (car x)) (pair-a (car y)))
-               (char<=? (pair-b (car x)) (pair-b (car y)))
-               (char<=? (pair-a (car x)) (pair-a (car y)))))
-          (else (<= (cdr x) (cdr y)))))))
-
-(define (print-pairs text pairs)
-  (format #t text)
-  (for-each
-    (lambda (x)
-        (format #t "~a,~a: ~a\n" (pair-a (car x)) (pair-b (car x)) (cdr x)))
-   (mysort pairs)))
-
-(define (compare mol mol2)
-  (let* (
-         (mol (molecule->molecule2 mol))
-         (pairs (molecule2-pairs mol))
-         (pairs2 (molecule2-pairs mol2)))
-     (for-each
-        (lambda (pair)
-            (let* ((val (cdr pair))
-                   (pair (car pair))
-                   (pair2 (or (assoc pair pairs2) (cons pair #f)))
-                   (val2 (cdr pair2))
-                   (pair (cons (pair-a pair) (pair-b pair))))
-              (format #t "~a: ~a ~a\n" pair val val2)))
-        pairs)))
-
-
 (define (molecule->molecule2 molecule)
  (let* ((pairs (count-elem
                  (zip molecule (cdr molecule))))
@@ -102,27 +51,6 @@
                  pairs))
         (last (list-ref molecule (1- (length molecule)))))
    (make-molecule2 pairs (car molecule) last)))
-
-(define (one-step molecule rules)
-  (let loop ((molecule molecule) (result '()))
-      (if (<= (length molecule) 1)
-          (reverse (cons (car molecule) result))
-          (let* ((a (car molecule))
-                 (b (cadr molecule))
-                 (n (assoc (cons a b) rules))
-                 (n (if n (cdr n) n))
-                 (result (cons a result))
-                 (result (if n (cons n result) result)))
-            (loop (cdr molecule) result)))))
-
-;;(define (decrease! k alist num)
-;;   (let* ((oldval (assoc k alist))
-;;          (oldval (if oldval (cdr oldval) #f))
-;;          (newval (if oldval (- oldval num) #f))
-;;          (newval (if (>= newval 1) newval #f)))
-;;     (if newval
-;;         (assoc-set! alist k newval)
-;;         (alist-delete! k alist))))
 
 (define (increase! k alist num)
    (let* ((oldval (assoc k alist)))
@@ -151,46 +79,35 @@
                   (loop (cdr pairs) result))
                (loop (cdr pairs) result)))))))
 
-(define (iterate-polymerisation2 molecule rules times)
-  (let loop ((times times) (molecule molecule))
-    (begin
-      (if (<= times 0)
-          molecule
-          (loop (1- times) (one-step2 molecule rules))))))
-
-(define (iterate-polymerisation molecule rules times)
-  (let loop ((times times) (molecule molecule))
-      (if (<= times 0)
-          molecule
-          (loop (1- times) (one-step molecule rules)))))
-
 (define (read-molecule port)
   (let* ((line (read-line port))
          (molecule (string->list line)))
     molecule))
 
-(define (line->rule line)
+(define (read-molecule2 port)
+ (let* ((line (read-line port))
+        (chars (string->list line))
+        (charpairs (zip chars (cdr chars)))
+        (countedpairs (fold
+                        (lambda (x acc)
+                           (increase! (make-pair (car x) (cadr x)) acc 1))
+                        '()
+                        charpairs))
+        (last (list-ref chars (1- (length chars))))
+        (first (car chars)))
+   (make-molecule2 countedpairs first last)))
+
+(define (line->rule2 line)
    (let* ((line line)
           (separator (string-contains line " -> "))
           (pair (substring line 0 separator))
-          (pair (cons (string-ref pair 0) (string-ref pair 1)))
+          (pair (make-pair-internal pair))
           (inserted (string-ref line (+ 4 separator))))
      (cons pair inserted)))
 
 (define (read-rules port)
   (let* ((bloc (read-bloc port))
-         (rules (map line->rule bloc)))
-      rules))
-
-(define (rules->rules2 rules)
-    (map
-      (lambda (x)
-        (begin
-          (let* ((pair (car x))
-                 (value (cdr x))
-                 (a (car pair))
-                 (b (cdr pair)))
-            (cons (make-pair a b) value))))
+         (rules (map line->rule2 bloc)))
       rules))
 
 (define (count-elem list)
@@ -207,14 +124,14 @@
   (let* ((counts (count-elem list)))
      (let loop ((counts counts) (least #f) (most #f))
          (if (null? counts) (cons least most)
-             (let* ((cur (car counts))
-                    (rest (cdr counts))
-                    (curcar (car cur))
-                    (curcount (cdr cur))
+             (let* ((cur        (car counts))
+                    (rest       (cdr counts))
+                    (curcar     (car cur))
+                    (curcount   (cdr cur))
                     (leastcount (if least (cdr least) 0))
-                    (mostcount (if most (cdr most) 0))
-                    (least (if (or (not least) (<= curcount leastcount)) cur least))
-                    (most (if (>= curcount mostcount) cur most)))
+                    (mostcount  (if most (cdr most) 0))
+                    (least      (if (or (not least) (<= curcount leastcount)) cur least))
+                    (most       (if (>= curcount mostcount) cur most)))
                 (loop rest least most))))))
 
 (define (most-minus-least list)
@@ -224,18 +141,12 @@
      (- mcount lcount)))
 
 (define (count-pairs->count-chars pairs first last)
- (let* ((char-count
-         (let loop ((pairs pairs) (chars '()))
-             (begin
-              (if (null? pairs)
-                  chars
-                  (let* ((cur (car pairs))
-                         (num (cdr cur))
-                         (cur (car cur))
-                         (a (pair-a cur))
-                         (chars (increase! a chars num))
-                         (pairs (cdr pairs)))
-                     (loop pairs chars))))))
+ (let* ((char-count (fold
+                      ;; count left char of each pair
+                      (lambda (x acc) (increase! (pair-a (car x)) acc (cdr x)))
+                      '()
+                      pairs))
+         ;; count last char as it is on left on no pair
         (char-count (increase! last char-count 1)))
    char-count))
 
@@ -251,14 +162,18 @@
 
 (define-public (main args)
   (let* ((port (current-input-port))
-         (polymer (read-molecule port))
-         (polymer2 (molecule->molecule2 polymer))
+         (polymer2 (read-molecule2 port))
          (emptyline (read-line port))
-         (rules (read-rules port))
-         (rules2 (rules->rules2 rules))
-         (polymer10 (iterate-polymerisation2 polymer2 rules2 10))
+         (rules2 (read-rules port))
+         (polymer10 (fold
+                      (lambda (_ polymer2) (one-step2 polymer2 rules2))
+                      polymer2
+                      (iota 10)))
          (result1 (molecule2->result polymer10))
-         (polymer40 (iterate-polymerisation2 polymer2 rules2 40))
+         (polymer40 (fold
+                      (lambda (_ polymer2) (one-step2 polymer2 rules2))
+                      polymer10
+                      (iota 30)))
          (result2 (molecule2->result polymer40)))
     (format #t "result1: ~a\n" result1)
     (format #t "result2: ~a\n" result2)))
