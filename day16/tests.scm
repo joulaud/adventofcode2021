252e97549eba9c30ab67415adc44b737d05b049d
Francois Joulaud <francois.joulaud@radiofrance.com>
2021-12-16 11:19:40 +0100

    day16: validate code with tests

--- day16/tests.scm
+++ day16/tests.scm
@@ -40,7 +40,7 @@
 (import-private stream->packet-result)
 (import-private result-sumversion)
 
-(test-begin "parsing input")
+(test-begin "sum-versions")
 (define (string->sumversion str)
    (let* ((strm-char (stream-of-chars (open-input-string str)))
           (stream-bits (stream-of-bits strm-char))
@@ -50,7 +50,28 @@
 (test-equal (car tests-sum-versions) (string->sumversion (car test-strings)))
 (test-equal (cadr tests-sum-versions) (string->sumversion (cadr test-strings)))
 (test-equal (caddr tests-sum-versions) (string->sumversion (caddr test-strings)))
-(test-end "parsing input")
+(test-end "sum-versions")
+
+(import-private result-other)
+
+(test-begin "apply-operation")
+(define (string->resultoperation str)
+   (let* ((strm-char (stream-of-chars (open-input-string str)))
+          (stream-bits (stream-of-bits strm-char))
+          (packet-result (stream->packet-result stream-bits))
+          (result (result-other packet-result)))
+     result))
+
+(test-equal  3 (string->resultoperation "C200B40A82"))
+(test-equal 54 (string->resultoperation "04005AC33890"))
+(test-equal  7 (string->resultoperation "880086C3E88112"))
+(test-equal  9 (string->resultoperation "CE00C43D881120"))
+(test-equal  1 (string->resultoperation "D8005AC2A8F0"))
+(test-equal  0 (string->resultoperation "F600BC2D8F"))
+(test-equal  0 (string->resultoperation "9C005AC2F8F0"))
+(test-equal  1 (string->resultoperation "9C0141080250320F1802104A08"))
+(test-end "apply-operation")
+
 
 ;; packet
 ;; 0123456789ABCDEF
