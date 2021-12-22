2f6750535e185d11fbebe224490d979626b60da0
Francois Joulaud <francois.joulaud@radiofrance.com>
2021-12-22 18:36:58 +0100

    day21 init

---
+++ day21/tests.scm
@@ -0,0 +1,27 @@
+(define-module (adventofcode2021 day21 tests)
+    #:use-module (adventofcode2021 day21 utils)
+    #:use-module (adventofcode2021 day21 dice)
+    #:use-module (ice-9 rdelim)
+    #:use-module (srfi srfi-1)
+    #:use-module (srfi srfi-2) ; and-let*
+    #:use-module (srfi srfi-19)
+    #:use-module (srfi srfi-41)
+    #:use-module (srfi srfi-43)
+    #:use-module (srfi srfi-64))
+
+(define (dbg t v) (format #t "~s: ~a\n" t v) (force-output))
+
+(define-syntax-rule (import-private module)
+    (define module (@@ (adventofcode2021 day21 dice) module)))
+
+(define (guess-current-dirname)
+  (and-let* ((source-location (current-source-location))
+             (filename (assq-ref source-location 'filename))
+             (absolute-filepath (%search-load-path filename))
+             (absolute-dirname (dirname absolute-filepath)))
+         absolute-dirname))
+
+(define testdir (or (guess-current-dirname) (getcwd)))
+
+;; import internal functions of module to test
+;(import-private parse-enhancement)
